db038f1f5ad55eaaa7348e01fcc48cee1ffc38e8
kuppermx <admin@kupper.mx>
2025-12-24 15:13:24 -0600

    Add debug logs for Auth0 config

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,6 +1,15 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 import { linkUserIdentity } from './identity-linker';
 
+console.log("Auth0 Config Check:", {
+    hasDomain: !!process.env.AUTH0_ISSUER_BASE_URL,
+    hasClientId: !!process.env.AUTH0_CLIENT_ID,
+    hasClientSecret: !!process.env.AUTH0_CLIENT_SECRET,
+    hasSecret: !!process.env.AUTH0_SECRET,
+    hasAppBaseUrl: !!process.env.AUTH0_BASE_URL,
+    appBaseUrl: process.env.AUTH0_BASE_URL, // Safe to log public URL
+});
+
 export const auth0 = new Auth0Client({
     domain: process.env.AUTH0_ISSUER_BASE_URL,
     clientId: process.env.AUTH0_CLIENT_ID,
