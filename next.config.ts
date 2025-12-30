1d491da2f3d6f9acba49c89becf164c6dba6d223
kuppermx <admin@kupper.mx>
2025-12-30 14:49:05 -0600

    Fix deployment: Enable output: standalone for OpenNext

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   basePath: process.env.BASE_PATH || "/app",
   assetPrefix: process.env.ASSET_PREFIX || "/app",
+  output: "standalone",
 };
 
 export default nextConfig;
