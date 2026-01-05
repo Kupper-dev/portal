8b74e85288f7c09c0fb086bd22531d899087d5f3
kuppermx <admin@kupper.mx>
2026-01-05 11:04:53 -0600

    chore: sync devlink components with recent UI fixes

--- src/devlink/DevicesTable.js
+++ src/devlink/DevicesTable.js
@@ -1,8 +1,13 @@
 "use client";
 import React from "react";
 import * as _Builtin from "./_Builtin";
+import * as _interactions from "./interactions";
 import { ProgressBar } from "./ProgressBar";
 import { StatusBadge } from "./StatusBadge";
+
+const _interactionsData = JSON.parse(
+  '{"events":{"e-11":{"id":"e-11","name":"","animationType":"custom","eventTypeId":"MOUSE_CLICK","action":{"id":"","actionTypeId":"GENERAL_START_ACTION","config":{"delay":0,"easing":"","duration":0,"actionListId":"a-9","affectedElements":{},"playInReverse":false,"autoStopEventId":"e-12"}},"mediaQueries":["main","medium","small","tiny"],"target":{"appliesTo":"ELEMENT","styleBlockIds":[],"id":"30873253-9fc0-0083-b6f8-6601c88e8d73"},"targets":[],"config":{"loop":false,"playInReverse":false,"scrollOffsetValue":null,"scrollOffsetUnit":null,"delay":null,"direction":null,"effectIn":null},"createdOn":1767632561057},"e-12":{"id":"e-12","name":"","animationType":"custom","eventTypeId":"MOUSE_SECOND_CLICK","action":{"id":"","actionTypeId":"GENERAL_START_ACTION","config":{"delay":0,"easing":"","duration":0,"actionListId":"a-10","affectedElements":{},"playInReverse":false,"autoStopEventId":"e-11"}},"mediaQueries":["main","medium","small","tiny"],"target":{"appliesTo":"ELEMENT","styleBlockIds":[],"id":"30873253-9fc0-0083-b6f8-6601c88e8d73"},"targets":[],"config":{"loop":false,"playInReverse":false,"scrollOffsetValue":null,"scrollOffsetUnit":null,"delay":null,"direction":null,"effectIn":null},"createdOn":1767632561058}},"actionLists":{"a-9":{"id":"a-9","title":"Row Open","actionItemGroups":[{"actionItems":[{"id":"a-9-n-3","actionTypeId":"STYLE_SIZE","config":{"delay":0,"easing":"","duration":500,"target":{"useEventTarget":true,"id":"7e9f016b-d977-bbc8-6141-c31db4de6a14"},"heightValue":4.26,"widthUnit":"PX","heightUnit":"rem","locked":false}}]},{"actionItems":[{"id":"a-9-n-2","actionTypeId":"STYLE_SIZE","config":{"delay":0,"easing":"outQuint","duration":800,"target":{"useEventTarget":true,"id":"7e9f016b-d977-bbc8-6141-c31db4de6a14"},"heightValue":100,"widthUnit":"PX","heightUnit":"%","locked":false}}]}],"useFirstGroupAsInitialState":true,"createdOn":1767459692598},"a-10":{"id":"a-10","title":"Row closes","actionItemGroups":[{"actionItems":[{"id":"a-10-n","actionTypeId":"STYLE_SIZE","config":{"delay":0,"easing":"outQuint","duration":800,"target":{"useEventTarget":true,"id":"7e9f016b-d977-bbc8-6141-c31db4de6a14"},"heightValue":4.25,"widthUnit":"PX","heightUnit":"rem","locked":false}}]}],"useFirstGroupAsInitialState":false,"createdOn":1767459878497}},"site":{"mediaQueries":[{"key":"main","min":992,"max":10000},{"key":"medium","min":768,"max":991},{"key":"small","min":480,"max":767},{"key":"tiny","min":0,"max":479}]}}'
+);
 
 export function DevicesTable({
   as: _Component = _Builtin.Block,
@@ -31,8 +36,14 @@ export function DevicesTable({
     href: "#",
   },
 }) {
+  _interactions.useInteractions(_interactionsData);
+
   return (
-    <_Component className="dynamic_row" tag="div">
+    <_Component
+      className="dynamic_row"
+      data-w-id="30873253-9fc0-0083-b6f8-6601c88e8d73"
+      tag="div"
+    >
       <_Builtin.Block className="table_row_2" tag="div">
         <_Builtin.Block
           className="table_cell"
