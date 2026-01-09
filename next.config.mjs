120d6ed8f246b7adc6c9a2d1d784efdeb25bf30f
kuppermx <admin@kupper.mx>
2026-01-08 21:03:39 -0600

    Convert next.config.ts to .mjs to improve framework detection

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 
-import type { NextConfig } from "next";
+
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 // Added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
 initOpenNextCloudflareForDev();
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   basePath: "/app",
   assetPrefix: "/app",
   output: "standalone",
